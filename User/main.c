d4cf42914569b6f6b65cfdb164014b37bf8c81bc
YINGYYUN <756209834@qq.com>
2025-10-22 12:59:19 +0800

    fix:2025.10.22更新了定时中断(TIM1)和非阻塞式按键(PA0单击)

--- User/main.c
+++ User/main.c
@@ -2,8 +2,8 @@
 #include "Delay.h"
 #include "Timer.h"
 #include "LED.h"
-#include "Key.h"
 #include "OLED.h"
+#include "Key.h"
 
 /*OLED测试*/
 //int main(void)
@@ -29,34 +29,64 @@
 
 
 /*LED测试*/
+//int main(void)
+//{
+//	LED_Init();
+//	
+//	while (1)
+//	{
+//		LED_ON();
+//		Delay_ms(500);
+//		LED_OFF();
+//		Delay_ms(500);
+//		LED_Turn();
+//		Delay_ms(500);
+//		LED_Turn();
+//		Delay_ms(500);
+//	}
+//}
+
+/*TIM1定时器定时中断和非阻塞式按键测试*/
+uint16_t i = 0;
+uint16_t j = 0;
+uint8_t KeyNum;
+
 int main(void)
 {
-	LED_Init();
+	OLED_Init();
+	Key_Init();
+	
+	Timer_Init();
 	
 	while (1)
 	{
-		LED_ON();
-		Delay_ms(500);
-		LED_OFF();
-		Delay_ms(500);
-		LED_Turn();
-		Delay_ms(500);
-		LED_Turn();
-		Delay_ms(500);
+		KeyNum = Key_GetNum();
+		if(KeyNum == 1)//PA-按下
+		{
+			j++;
+		}
+		OLED_Printf(0,0,OLED_8X16,"i:%05d",i);
+		OLED_Printf(0,16,OLED_8X16,"i:%05d",j);
+		
+		OLED_Update();
 	}
 }
 
-
-
-void TIM2_IRQHandler(void)
-{
-	if (TIM_GetITStatus(TIM2,TIM_IT_Update) == SET )
-	{
-		//由定时器中断自动执行;有利于多模块共用定时器定时
+//由定时器中断自动执行;有利于多模块共用定时器定时
 		//同时，需要防止中断重叠
 		//一:减小模块内中断函数的内容，减小运行时间
 		//二：增加定时器的基础时间
+void TIM1_UP_IRQHandler(void)
+{
+	//检查标志位
+	if (TIM_GetITStatus(TIM1,TIM_IT_Update) == SET )
+	{
+		i++;
+		
+		//用于Key模块的内部检测
 		Key_Tick();
-		TIM_ClearITPendingBit(TIM2,TIM_IT_Update);
+		//清除标志位
+		TIM_ClearITPendingBit(TIM1,TIM_IT_Update);
 	}
 }
+
